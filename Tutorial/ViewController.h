d3b19f168c271585651f4e834f279ecdb8136d07
ebanos <edanos@ids.com.mx>
2017-02-21 09:54:05 -0600

    Initial Commit

---
+++ Tutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tutorial
+//
+//  Created by Beca IDS on 21/02/17.
+//  Copyright © 2017 BecaIDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tutorial/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tutorial
+//
+//  Created by Beca IDS on 21/02/17.
+//  Copyright © 2017 BecaIDS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
